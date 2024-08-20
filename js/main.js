6b92cda9b64306bf73b5a9d146e075ca7be22640
Keln <EgorKeln@narod.ru>
2024-08-20 09:54:10 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -54,21 +54,6 @@ close7.onclick = function(){
 	hamburger.classList.toggle('is-open');
 };
 
-
-
-/*let login = document.querySelector('.login');
-let create = document.querySelector('.create');
-let container = document.querySelector('.container');
-
-login.onclick = function(){
-    container.classList.add('signinForm')
-}
-create.onclick = function(){
-    container.classList.remove('signinForm')
-};*/
-
-
-
 const SkewedOne = document.querySelector('.SkewedOne');
 const SkewedTwo = document.querySelector('.SkewedTwo');
 window.addEventListener('scroll', function(){
@@ -78,61 +63,4 @@ window.addEventListener('scroll', function(){
     SkewedTwo.style.transform = "skewY(" + value2 + "deg)";
 });
 
-
-
-let cardOnClickOne = document.querySelector('.cardOnClickOne');
-let OpenOne = document.querySelector('.OpenOne');
-let CloseOne = document.querySelector('.CloseOne');
-let cardOnClickTwo = document.querySelector('.cardOnClickTwo');
-let OpenTwo = document.querySelector('.OpenTwo');
-let CloseTwo = document.querySelector('.CloseTwo');
-let cardOnClickThree = document.querySelector('.cardOnClickThree');
-let OpenThree = document.querySelector('.OpenThree');
-let CloseThree = document.querySelector('.CloseThree');
-OpenOne.onclick = function(){
-    cardOnClickOne.classList.add('activeСardOne');
-    cardOnClickTwo.classList.remove('activeСardTwo');
-    cardOnClickThree.classList.remove('activeСardThree');
-}
-OpenTwo.onclick = function(){
-    cardOnClickTwo.classList.add('activeСardTwo');
-    cardOnClickOne.classList.remove('activeСardOne');
-    cardOnClickThree.classList.remove('activeСardThree');
-}
-OpenThree.onclick = function(){
-    cardOnClickThree.classList.add('activeСardThree');
-    cardOnClickOne.classList.remove('activeСardOne');
-    cardOnClickTwo.classList.remove('activeСardTwo');
-}
-CloseOne.onclick = function(){
-    cardOnClickOne.classList.remove('activeСardOne');
-}
-CloseTwo.onclick = function(){
-    cardOnClickTwo.classList.remove('activeСardTwo');
-}
-CloseThree.onclick = function(){
-    cardOnClickThree.classList.remove('activeСardThree');
-};
-
-
-
-jQuery(document).ready(function($) {
-	$("#contact").submit(function() {
-		var str = $(this).serialize();
-		$.ajax({
-			type: "POST",
-			url: "../mail.php",
-			data: str,
-			success: function(msg) {
-				if(msg == 'OK') {
-					result = '<div class="ok">Сообщение отправлено</div>';
-					$("#fields").hide();
-				}
-				else {result = msg;}
-				$('#note').html(result);
-			}
-		});
-		return false;
-	});
-});
 
